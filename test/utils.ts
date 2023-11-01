af80e0650a3f565c500d821148b70ee247d364fd
Gabriele D'Arrigo <darrigo.g@gmail.com>
2023-11-01 19:48:47 +0100

    Introduce a createMock utility

---
+++ test/utils.ts
@@ -0,0 +1,31 @@
+type NonMockableTypes =
+  | Number
+  | Boolean
+  | String
+  | Symbol
+  | BigInt
+  | Date
+  | RegExp
+  | Generator;
+
+type MockedFn<A extends any[], R> = ((...args: A) => R) | jest.Mock<R, A>;
+
+type MockObject<T> = {
+  [K in keyof T]?: MockOverrides<T[K]>;
+};
+
+type MockOverrides<T> = T extends NonMockableTypes
+  ? T
+  : T extends Array<infer ItemType>
+  ? MockOverrides<ItemType>[]
+  : T extends InstanceType<any>
+  ? MockObject<T>
+  : T extends (...args: infer Args) => infer ReturnType
+  ? MockedFn<Args, ReturnType>
+  : T extends object
+  ? MockObject<T>
+  : never;
+
+export function createMock<T extends object>(overrides?: MockOverrides<T>): T {
+  return (overrides || {}) as T;
+}
